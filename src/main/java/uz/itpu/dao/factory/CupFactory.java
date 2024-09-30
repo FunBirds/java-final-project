c2f32726796cc4bc8def04f09ea23ece584053b2
abdullajonov.uz <abdullajonovboburjon05@gmail.com>
2024-09-30 20:33:56 +0500

    [upd] Factory Pattern Applied

--- src/main/java/uz/itpu/dao/factory/CupFactory.java
+++ src/main/java/uz/itpu/dao/factory/CupFactory.java
@@ -3,7 +3,6 @@ package uz.itpu.dao.factory;
 import uz.itpu.dao.CupDAO;
 import uz.itpu.dao.DAOFactory;
 import uz.itpu.dao.DAOInterface;
-import uz.itpu.entity.Cup;
 import uz.itpu.entity.Tableware;
 
 import java.sql.SQLException;
